1fcd91ebb2940b5cea7645e29cfe30e2be29dab9
tangkaiqiang <justforgit@163.com>
2020-03-18 15:27:56 +0800

    1

--- React/react-demo/README.md
+++ React/react-demo/README.md
@@ -12,3 +12,7 @@
 + `static getDerivedStateFromError()`,`componentDidCatch()`
 + 可以将其包装在最顶层的路由组件并为用户展示一个 “Something went wrong” 的错误信息
 
+## Fragment
++ `Fragments` 允许你将子列表分组，而无需向 DOM 添加额外节点。
++ `key` 是唯一可以传递给 `Fragment` 的属性。
+
--- React/react-demo/src/components/ref/index.js
+++ React/react-demo/src/components/ref/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+const FancyButton = React.forwardRef((props, ref) => {
+    console.log(ref)
+    return (
+        <button ref={ref}>
+            {props.children}
+        </button>
+    )
 
-export default class RefComponent extends React.Component{
-    render(){
-        return(
-            <div>ref</div>
+})
+const ref = React.createRef()
+
+export default class RefComponent extends React.Component {
+    render() {
+        return (
+            <FancyButton ref={ref}>Click me</FancyButton>
         )
     }
 }
